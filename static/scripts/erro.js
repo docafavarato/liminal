fe6cf1f27d55fa5eb6445e475f70e00ffbf10f0e
São Francisco Vet <docafavarato@gmail.com>
2023-12-01 14:44:14 -0300

    error overlay

--- static/scripts/erro.js
+++ static/scripts/erro.js
@@ -2,14 +2,24 @@ function mostrarErro() {
   var audio = document.getElementById('click');
   audio.play();
 
-  var overlay = document.getElementById('erroOverlay');
+
+  var overlay = document.getElementById('overlay');
   overlay.style.display = 'block';
+
+
+  var erroOverlay = document.getElementById('erroOverlay');
+  erroOverlay.style.display = 'block';
 }
 
 function fecharErro() {
   var audio = document.getElementById('click');
   audio.play();
-  
-  var overlay = document.getElementById('erroOverlay');
+
+
+  var overlay = document.getElementById('overlay');
   overlay.style.display = 'none';
+
+
+  var erroOverlay = document.getElementById('erroOverlay');
+  erroOverlay.style.display = 'none';
 }
