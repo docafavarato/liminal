e4e0e7653fa87ac9f4f912616d4703e827ddf4ab
docafavarato <docafavarato@gmail.com>
2024-04-25 02:47:43 -0300

    update

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import os
 from flask import Flask, render_template, url_for, redirect
 import json
 import random
+import string
 
 app = Flask(__name__)
+
+def randomString(range1, range2):
+    length = random.randint(range1, range2)
+    return str(''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase +
+                             string.digits, k=length)))
 
 @app.route("/", methods=["GET"])
 def index():
@@ -21,7 +27,7 @@ def desktop():
     songs = [file for file in files]
     random.shuffle(songs)
   
-    return render_template("desktop.html", spaces=spaces, songs=songs)
+    return render_template("desktop.html", spaces=spaces, songs=songs, randomString=randomString)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/scripts/click.js
+++ static/scripts/click.js
@@ -1,6 +1,15 @@
 function playSound(filename) {
     var audio = document.getElementById(filename);
     audio.play();
+
+    ignore = ["click", "error", "pop-up-blocked"]
+
+    if (!ignore.includes(filename)) {
+        document.querySelector("#toast .toast-body").textContent = "Now playing: " + filename;
+
+        const toast = bootstrap.Toast.getOrCreateInstance(document.getElementById('toast'));
+        toast.show();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
