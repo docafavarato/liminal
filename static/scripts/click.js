2c332f77e2d475bc8b2c26769a937728a1fc5f9b
docafavarato <docafavarato@gmail.com>
2024-04-27 19:06:50 -0300

    feat: multiple modals at the same time

--- static/scripts/click.js
+++ static/scripts/click.js
@@ -1,3 +1,65 @@
+function playMario() {
+    document.getElementById("mario").style.display = "block";
+    document.getElementById("mario").play();
+    document.getElementById("trumpet").play();
+    document.body.style.backgroundImage = 'url("../static/styles/images/ty26hmpfe3f51.jpg")';
+    document.body.style.backgroundRepeat = "repeat";
+    document.body.style.backgroundSize = "80px 80px";
+    document.getElementById("error-sound").play();
+
+    setTimeout(function() {
+        window.close();
+    }, 5800);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var modals = document.querySelectorAll(".modal");
+    
+    modals.forEach(modal => {
+        $(modal).on("shown.bs.modal", function () {
+            var mouseX = event.clientX;
+            var mouseY = event.clientY;
+
+            var rand = randomIntFromInterval(1, 2);
+            switch (rand) {
+                case 1:
+                    modal.style.left = (mouseX - 20) + "px";
+                    modal.style.top = (mouseY - 20) + "px";
+                    break;
+                case 2:
+                    modal.style.left = (mouseX + 200) + "px";
+                    modal.style.top = (mouseY - 20) + "px";
+            }
+        });
+    });
+});
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modals = document.getElementsByClassName("modal");
+
+    for (var i = 0; i < modals.length; i++) {
+        ["click", "mousedown"].forEach(ev => {
+            modals[i].addEventListener(ev, function() {
+                var maiorZIndex = 0;
+                var modals = document.getElementsByClassName("modal");
+    
+                for (var j = 0; j < modals.length; j++) {
+                    var zIndex = parseInt(window.getComputedStyle(modals[j]).getPropertyValue('z-index'));
+                    if (!isNaN(zIndex) && zIndex > maiorZIndex) {
+                        maiorZIndex = zIndex;
+                    }
+                }
+    
+                this.style.zIndex = maiorZIndex + 1;
+            });
+        });
+    }
+});
+
 function playSound(filename) {
     var audio = document.getElementById(filename);
     audio.play();
